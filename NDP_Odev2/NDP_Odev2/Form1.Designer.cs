e3a2a4cda03498bad5364917e78140e280305ef3
selinnurgol <selinnur.gol@ogr.sakarya.edu.tr>
2020-04-10 03:34:26 +0300

    NDP Odev 2

---
+++ NDP_Odev2/NDP_Odev2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace NDP_Odev2
+{
+    partial class AnaPanel
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xLabel = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.arkadasButton = new System.Windows.Forms.Button();
+            this.sonButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(34, 50);
+            this.xLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(24, 20);
+            this.xLabel.TabIndex = 0;
+            this.xLabel.Text = "X:";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(85, 48);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(209, 26);
+            this.xTextBox.TabIndex = 1;
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(85, 94);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(209, 26);
+            this.yTextBox.TabIndex = 3;
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Location = new System.Drawing.Point(34, 96);
+            this.yLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(24, 20);
+            this.yLabel.TabIndex = 2;
+            this.yLabel.Text = "Y:";
+            // 
+            // arkadasButton
+            // 
+            this.arkadasButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.arkadasButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.arkadasButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.arkadasButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.arkadasButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.arkadasButton.Location = new System.Drawing.Point(38, 146);
+            this.arkadasButton.Name = "arkadasButton";
+            this.arkadasButton.Size = new System.Drawing.Size(102, 32);
+            this.arkadasButton.TabIndex = 4;
+            this.arkadasButton.Text = "ARKADAŞ MI?";
+            this.arkadasButton.UseVisualStyleBackColor = false;
+            this.arkadasButton.Click += new System.EventHandler(this.arkadasButton_Click);
+            // 
+            // sonButton
+            // 
+            this.sonButton.BackColor = System.Drawing.Color.Crimson;
+            this.sonButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.sonButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sonButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sonButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.sonButton.Location = new System.Drawing.Point(192, 146);
+            this.sonButton.Name = "sonButton";
+            this.sonButton.Size = new System.Drawing.Size(102, 32);
+            this.sonButton.TabIndex = 5;
+            this.sonButton.Text = "SON";
+            this.sonButton.UseVisualStyleBackColor = false;
+            this.sonButton.Click += new System.EventHandler(this.sonButton_Click);
+            // 
+            // AnaPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(346, 211);
+            this.Controls.Add(this.sonButton);
+            this.Controls.Add(this.arkadasButton);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.xLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AnaPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arkadaş Sayılar";
+            this.Load += new System.EventHandler(this.AnaPanel_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.Button arkadasButton;
+        private System.Windows.Forms.Button sonButton;
+    }
+}
+
---
+++ NDP_Odev2/NDP_Odev2/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDP_Odev2
+{
+    public partial class AnaPanel : Form
+    {
+        public int x = 0, y = 0, sayac = 0;
+        ArrayList xBasamaklari;
+        ArrayList yBasamaklari;
+        int toplamX;
+        int toplamY;
+        Label xListBoxLabel;
+        Label yListBoxLabel;
+        ListBox xListBox;
+        ListBox yListBox;
+        Label toplamlarLabel;
+        TextBox xTextBoxList;
+        TextBox yTextBoxList;
+
+        public AnaPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void sonButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void arkadasButton_Click(object sender, EventArgs e)
+        {
+            if (sayac == 0)
+            {
+
+                if (xTextBox.Text != "" && yTextBox.Text != "")
+                {
+                    this.x = Convert.ToInt32(xTextBox.Text);
+                    this.y = Convert.ToInt32(yTextBox.Text);
+
+                    this.Width = 800;
+                    this.Height = 500;
+
+                    xListBoxLabel = new Label();
+                    xListBoxLabel.Size = new Size(24, 20);
+                    xListBoxLabel.Location = new Point(423, 50);
+                    xListBoxLabel.Text = "X:";
+                    this.Controls.Add(xListBoxLabel);
+
+                    yListBoxLabel = new Label();
+                    yListBoxLabel.Size = new Size(24, 20);
+                    yListBoxLabel.Location = new Point(623, 50);
+                    yListBoxLabel.Text = "Y:";
+                    this.Controls.Add(yListBoxLabel);
+
+                    xListBox = new ListBox();
+                    xListBox.Size = new Size(120, 275);
+                    xListBox.Location = new Point(375, 70);
+                    this.Controls.Add(xListBox);
+
+                    yListBox = new ListBox();
+                    yListBox.Size = new Size(120, 275);
+                    yListBox.Location = new Point(575, 70);
+                    this.Controls.Add(yListBox);
+
+                    toplamlarLabel = new Label();
+                    toplamlarLabel.Size = new Size(75, 20);
+                    toplamlarLabel.Location = new Point(300, 375);
+                    toplamlarLabel.Text = "TOPLAMLAR:";
+                    this.Controls.Add(toplamlarLabel);
+
+
+                    xTextBoxList = new TextBox();
+                    xTextBoxList.Size = new Size(120, 20);
+                    xTextBoxList.Location = new Point(375, 375);
+                    this.Controls.Add(xTextBoxList);
+
+                    yTextBoxList = new TextBox();
+                    yTextBoxList.Size = new Size(120, 20);
+                    yTextBoxList.Location = new Point(575, 375);
+                    this.Controls.Add(yTextBoxList);
+
+
+
+                    xBasamaklari = new ArrayList();
+                    yBasamaklari = new ArrayList();
+
+                    for (int i = 1; i < x; i++)
+                    {
+                        if (x % i == 0)
+                        {
+                            xBasamaklari.Add(i);
+                        }
+                    }
+
+                    for (int i = 1; i < y; i++)
+                    {
+                        if (y % i == 0)
+                        {
+                            yBasamaklari.Add(i);
+                        }
+                    }
+
+
+                    foreach (var item in xBasamaklari)
+                    {
+                        xListBox.Items.Add(item);
+                    }
+
+                    foreach (var item in yBasamaklari)
+                    {
+                        yListBox.Items.Add(item);
+                    }
+
+
+                    toplamX = 0;
+
+                    foreach (var item in xListBox.Items)
+                    {
+                        toplamX = toplamX + Convert.ToInt32(item);
+                        xTextBoxList.Text = Convert.ToString(toplamX);
+                    }
+
+
+                    toplamY = 0;
+
+                    foreach (var item in yListBox.Items)
+                    {
+                        toplamY = toplamY + Convert.ToInt32(item);
+                        yTextBoxList.Text = Convert.ToString(toplamY);
+                    }
+
+                    if (x==toplamY && y==toplamX)
+                    {
+                        MessageBox.Show("Arkadaş sayıdırlar.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Arkadaş sayı değildirler.");
+                    }
+                    sayac++;
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bütün textboxları doldurunuz!");
+                }
+                
+            }
+
+            else
+            {
+                if (xTextBox.Text != "" && yTextBox.Text != "")
+                {
+                    xListBox.Items.Clear();
+                    yListBox.Items.Clear();
+                    xTextBoxList.Text = "";
+                    yTextBoxList.Text = "";
+
+                    this.x = Convert.ToInt32(xTextBox.Text);
+                    this.y = Convert.ToInt32(yTextBox.Text);
+
+                    xBasamaklari = new ArrayList();
+                    yBasamaklari = new ArrayList();
+
+                    for (int i = 1; i < x; i++)
+                    {
+                        if (x % i == 0)
+                        {
+                            xBasamaklari.Add(i);
+                        }
+                    }
+
+                    for (int i = 1; i < y; i++)
+                    {
+                        if (y % i == 0)
+                        {
+                            yBasamaklari.Add(i);
+                        }
+                    }
+
+
+                    foreach (var item in xBasamaklari)
+                    {
+                        xListBox.Items.Add(item);
+                    }
+
+                    foreach (var item in yBasamaklari)
+                    {
+                        yListBox.Items.Add(item);
+                    }
+
+
+                    toplamX = 0;
+
+                    foreach (var item in xListBox.Items)
+                    {
+                        toplamX = toplamX + Convert.ToInt32(item);
+                        xTextBoxList.Text = Convert.ToString(toplamX);
+                    }
+
+
+                    toplamY = 0;
+
+                    foreach (var item in yListBox.Items)
+                    {
+                        toplamY = toplamY + Convert.ToInt32(item);
+                        yTextBoxList.Text = Convert.ToString(toplamY);
+                    }
+
+                    if (x == toplamY && y == toplamX)
+                    {
+                        MessageBox.Show("Arkadaş sayıdırlar.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Arkadaş sayı değildirler.");
+                    }
+                    sayac++;
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bütün textboxları doldurunuz!");
+                }
+                
+            }
+
+
+        }
+
+
+    }
+}
